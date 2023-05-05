6f1a90cacbb6c9258205f2cc29de239141e8fa51
ItunuoluwaOlowoye <itunu.owo@gmail.com>
2023-05-05 10:13:43 +0100

    create table

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 snowflake_username = 'Itee'
 snowflake_password = 'Adenike@16'
-snowflake_account = 'qgrnfkj-mj51774'
+snowflake_account = 'mj51774'
 snowflake_warehouse = 'COMPUTE_WH'
 snowflake_database = 'EMPLOYEE_DATA'
 snowflake_schema = 'PUBLIC'
