a4c5df776105e03b7aac9819b19ec09604bbd29f
ItunuoluwaOlowoye <itunu.owo@gmail.com>
2023-05-05 10:32:12 +0100

    e

--- create_db.py
+++ create_db.py
@@ -7,21 +7,9 @@ snowflake_account = 'qgrnfkj-mj51774'
 snowflake_warehouse = 'COMPUTE_WH'
 snowflake_database = 'EMPLOYEE_DATA'
 snowflake_schema = 'PUBLIC'
-
-engine = create_engine(
- 'snowflake://{user}:{password}@{account}/{db}/{schema}?warehouse={warehouse}'
- .format(
-     user=snowflake_username,
-     password=snowflake_password,
-     account=snowflake_account,
-     db=snowflake_database,
-     schema=snowflake_schema,
-     warehouse=snowflake_warehouse,
-    ),echo_pool=True, pool_size=10, max_overflow=20
-)
-
-connection = engine.connect()
 df_sensor = pd.read_csv('employee_data.csv')
+
+connection = st.experimental_connection('snowflake', type='sql')
 
 try:
     df_sensor.columns = map(str.upper, df_sensor.columns)
@@ -32,4 +20,3 @@ try:
 
 finally: 
     connection.close()
-    engine.dispose()
