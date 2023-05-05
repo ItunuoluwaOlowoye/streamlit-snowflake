d1ea881a42b918d5d5333de7bfe8bdf1877217a2
ItunuoluwaOlowoye <itunu.owo@gmail.com>
2023-05-05 13:21:57 +0100

    e

--- create_db.py
+++ create_db.py
@@ -1,20 +1,15 @@
 import streamlit as st
-from sqlalchemy import create_engine
 import pandas as pd
-snowflake_username = 'Itee'
-snowflake_password = 'Adenike@16'
-snowflake_account = 'qgrnfkj-mj51774'
-snowflake_warehouse = 'COMPUTE_WH'
-snowflake_database = 'EMPLOYEE_DATA'
-snowflake_schema = 'PUBLIC'
+from snowflake.connector.pandas_tools import write_pandas
+from snowflake.connector import connect
 
 df_sensor = pd.read_csv('employee_data.csv')
 
 connection = st.experimental_connection('snowflake', type='sql')
 
-import pandas
-from snowflake.connector.pandas_tools import pd_writer
+cnx = connect(user='Itee', password='Adenike@16', account='qgrnfkj-mj51774', 
+              database='EMPLOYEE_DATA', schema='PUBLIC', warehouse='COMPUTE_WH', role='ACCOUNTADMIN')
 
-df_sensor.to_sql('employees', con=connection, schema='PUBLIC', index=False, method=pd_writer)
-results = connection.execute('select count(1) from tb_equipments').fetchone()
-st.write(results[0])
+# Write the data from the DataFrame to the table named "employees".
+success, nchunks, nrows, _ = write_pandas(cnx, df_sensor, 'employees')
+st.success('success!')
