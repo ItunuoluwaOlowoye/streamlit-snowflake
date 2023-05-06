7a42171038855c8b5e2a35ba4b56f8c2772b49b0
ItunuoluwaOlowoye <itunu.owo@gmail.com>
2023-05-06 03:15:24 +0100

    ts

---
+++ departments.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import streamlit as st
+import os
+from functions import page_intro, get_img_with_href # collection of user defined functions
+
+st.set_page_config(page_title='BuyMart Department Data Management', page_icon=Image.open('pictures/browser-tab-logo.png'), layout='wide') # set the page layout
+
+page_intro('BuyMart Department Data Management Studio','Welcome to the BuyMart department data management studio. Please select your group below.')
+
+stack1, stack2, stack3 = st.columns([1.2,1,1.2]) # stack of different groups
+
+# link all pictures to url
+html_dict = {}
+for picture in os.listdir('pictures'):
+    html_dict[picture[:-4]] = get_img_with_href('pictures/'+picture, f'https://buy-mart-{picture[:-4]}.streamlit.app/')
+
+# add hyperlinked pictures to page
+stack1.markdown(html_dict['department-managers'], unsafe_allow_html=True)
+stack2.markdown(html_dict['program-managers'], unsafe_allow_html=True)
