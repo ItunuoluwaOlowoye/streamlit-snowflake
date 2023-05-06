c88e3f89a48ec854690389ec7d38391c393cd884
ItunuoluwaOlowoye <itunu.owo@gmail.com>
2023-05-06 09:04:08 +0100

    login cred

--- department_managers.py
+++ department_managers.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title='BuyMart Departments Portal', page_icon=browser_ta
 
 placeholder = st.empty() # create a main content placeholder
 with placeholder.container(): # create a container within the placeholder
-    sb_placeholder = functions.page_intro('Departments Portal',f"This portal is only accessible to department managers. This is where attendance for Sunday operations is logged by HR. You can find the list of usernames and passwords [here](https://docs.google.com/spreadsheets/d/1xLFmSlsziJEqIaiyN-1XO5Ew2UhZCV0x4D9_TKm9uuk/edit?usp=sharing)") # write the default page elements and store sidebar placeholder in a variable
+    sb_placeholder = functions.page_intro('Departments Portal',f"This portal is only accessible to department managers. This is where attendance for Sunday operations is logged by HR. You can find the list of usernames and passwords [here](https://docs.google.com/spreadsheets/d/1xLFmSlsziJEqIaiyN-1XO5Ew2UhZCV0x4D9_TKm9uuk/edit#gid=694321233)") # write the default page elements and store sidebar placeholder in a variable
 
 if functions.authenticate_user(placeholder,sb_placeholder):
     if st.session_state.user.groups.filter(name__in=["Department Managers"]).exists(): # after authentication and confirming that user is a service dept head
